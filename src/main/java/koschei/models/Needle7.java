811a8e8d7e21b90fced175b13cb0829d2c734f2a
a_khoruzhenko <khoruzhenko-ad@dep124.avt>
2023-11-28 16:03:27 +0500

    bean in config, setter and autowired

--- src/main/java/koschei/models/Needle7.java
+++ src/main/java/koschei/models/Needle7.java
@@ -1,7 +1,17 @@
 package koschei.models;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class Needle7 {
 
+
+    private Deth8 death;
+    @Autowired
+    public Needle7(Deth8 deth8) {
+        this.death = deth8;
+    }
     @Override
     public String toString() {
         return ", смерть Кощея на игле :( " + "";
