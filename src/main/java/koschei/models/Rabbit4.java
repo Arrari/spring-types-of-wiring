3ee99c557dd580cc499c698a860cd163ef1d76da
a_khoruzhenko <khoruzhenko-ad@dep124.avt>
2023-11-30 08:36:29 +0500

    Added private modifier

--- src/main/java/koschei/models/Rabbit4.java
+++ src/main/java/koschei/models/Rabbit4.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class Rabbit4 {
 
-    Duck5 duck;
+    private Duck5 duck;
     @Override
     public String toString() {
         return ", в зайце утка " + duck.toString();
